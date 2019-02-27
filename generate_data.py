573a68bf1f3ab7d5a7d1f717838791128868341a
Alan Plascinskas <alanas.plascinskas@gmail.com>
2019-02-27 21:31:44 +0000

    generate_data.py takes number of time steps argument from cmd

--- generate_data.py
+++ generate_data.py
@@ -1,3 +1,4 @@
+import sys
 import multiprocessing  as multiproc
 import custom_logger
 import datetime
@@ -9,10 +10,12 @@ if __name__ == '__main__':
 
     start_time = datetime.datetime.utcnow()
 
-    logger = custom_logger.get_logger('Data Generation')
+    logger = custom_logger.get_logger('Data_Generation')
     logger.info('Starting data generation...')
 
     num_time_steps = 1000
+    if len(sys.argv) > 1:
+        num_time_steps = int(sys.argv[1])
 
     # (std, num_vnf_profiles, num_time_steps, output_file_prefix)
     params = [(0.1, 750, num_time_steps, True), (0.1, 1000, num_time_steps, True), (0.1, 1250, num_time_steps, True),
