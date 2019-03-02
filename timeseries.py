Memory overflow issue fixed

timeseries.py
@@ -12,6 +12,7 @@ class TSEntry:
 
 class TimeSeries:
     # if file_prefix provided, loads data from file file_prefixT upon request for time series at time T
+    # max_time only needed with file_prefix
     def __init__(self, init_entry, file_prefix=None, max_time=1000):
         self.file_prefix = file_prefix
         if file_prefix is not None:
@@ -27,21 +28,26 @@ class TimeSeries:
         self.shape = init_entry.shape
         self.time_elapsed = 0
 
-
     def add_entry_delta(self, delta):
-        new_entry = TSEntry(utils.cap_profiles(self.last.entry+delta), self.time_elapsed+1)
-        new_entry.prev = self.last
-        self.last.next = new_entry
-
-        self.last = new_entry
-        self.time_elapsed += 1
+        new_entry = TSEntry(utils.cap_profiles(self.current.entry+delta), self.time_elapsed+1)
+        self.add_entry(new_entry)
 
     def add_entry(self, entry):
-        self.last.next = entry
-        self.last = entry
-        self.time_elapsed += 1
+        if self.file_prefix:
+            self.current = entry
+            self.time_elapsed = entry.time
+            with open(self.file_prefix + str(entry.time), 'wb') as data_file:
+                data_file.write(entry.entry.tostring())
+        else:
+            entry.prev = self.last
+            self.last.next = entry
+            self.last = entry
+            self.current = entry
+            self.time_elapsed += 1
 
     def load_entry(self, delta=None, time=None):
+        if self.file_prefix is None:
+            raise Exception('Entry loading is only for disk-based time series!')
         if delta is not None:
             # could cache next delta file here
             time = self.time_elapsed + delta
@@ -51,6 +57,9 @@ class TimeSeries:
             self.current = None
         else:
             with open(self.file_prefix + str(time), 'rb') as data_file:
+                if time == 0:
+                    data_file.readline()
+                    data_file.readline()
                 ts_entry = TSEntry(np.reshape(np.fromstring(data_file.read()), (-1, 3)), time)
                 self.current = ts_entry
                 self.time_elapsed = time

ztorch_simulation.py
@@ -79,24 +79,26 @@ class Simulation:
                 pathlib.Path(output_file).mkdir(parents=True, exist_ok=True)
                 output_file += '/ztorch_out'
 
-            # timeseries of each vnf compute needs
-            self.time_series = TimeSeries(init_profiles)
-
             data_file = None
             if output_file:
                 with open(output_file + '0', 'wb') as data_file:
                     data_file.write(bytes('num_vnf_profiles {}\n'.format(len(init_profiles)), encoding='UTF-8'))
                     data_file.write(bytes('num_time_steps {}\n'.format(steps), encoding='UTF-8'))
-                    data_file.write(self.time_series.first.entry.tostring())
+                    data_file.write(init_profiles.tostring())
 
-            for step in range(1, steps+1):
+            # time series of each vnf compute needs
+            if output_file:
+                self.time_series = TimeSeries(init_profiles, file_prefix=output_file, max_time=self.total_steps)
+            else:
+                self.time_series = TimeSeries(init_profiles)
+
+            for step in range(1, self.total_steps+1):
                 if step % 1000 == 0:
                     self.logger.info('Generating {}th step of time series...'.format(step))
                 delta = np.random.normal(0, std, self.time_series.shape)
                 self.time_series.add_entry_delta(delta)
-                if output_file:
-                    with open(output_file + str(step), 'wb') as data_file:
-                        data_file.write(self.time_series.last.entry.tostring())
+
+            self.time_series.load_entry(time=0)
 
         self.logger.info('Time series initialized!')
 